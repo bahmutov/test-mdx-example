cdd04ed45f832ef519c7b12aef725e767f486941
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-08 09:31:00 -0400

    works with lazy component, close #1

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,7 +4,7 @@ import {mount} from 'cypress-react-unit-test'
 
 describe('App', () => {
   // https://github.com/bahmutov/test-mdx-example/issues/1
-  it.skip('renders', () => {
+  it.only('renders', () => {
     mount(<App />)
     cy.get('[data-cy=demo]')
       .should('have.text', 'This is my demo')
