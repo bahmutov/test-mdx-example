5823fe8431dfe3a0eb212a46a6b3f63dcf49727d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-08 08:18:34 -0400

    add failing test with lazy import

---
+++ src/App.spec.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from './App'
+import {mount} from 'cypress-react-unit-test'
+
+describe('App', () => {
+  // https://github.com/bahmutov/test-mdx-example/issues/1
+  it.skip('renders', () => {
+    mount(<App />)
+    cy.get('[data-cy=demo]')
+      .should('have.text', 'This is my demo')
+  })
+})
